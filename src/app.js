a77300b3376e59ecbc98b8e4d60c73187adf9ed7
ogican5 <bagriyanikogulcan@gmail.com>
2025-09-25 22:16:29 +0300

    yenilik

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>❤️ Sürpriz Aşk Projesi Yayında! ❤️</h1>
+      <p>GitHub Pages ile yayınlandı.</p>
+    </div>
+  );
+}
+
+export default App;
